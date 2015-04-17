6a1e4a5aac454dffee3f004b9fd3b2c7cb6ef444
ebara10 <chihiro.ebara@net-marketing.co.jp>
2015-04-17 19:27:48 +0900

    -wの機能チェック

---
+++ is_check_func.sh
@@ -0,0 +1,37 @@
+#!/bin/bash
+fn="/backup/"
+testfile="test.txt"
+
+func() {
+if [ -w $fn ] ; then
+    echo "書き込みOK"
+else
+    echo "書き込みNG"
+fi
+touch ${fn}${testfile}
+echo ""
+}
+
+
+sudo rm -rf ${fn}
+echo "フォルダが無い場合"
+func
+
+sudo mkdir ${fn}
+echo "所有権・権限が無い場合"
+func
+
+sudo chmod -R 644 ${fn}
+sudo chown -R ebara:netmarketing ${fn}
+echo "所有権はあっても権限が無い場合"
+func
+
+sudo chmod -R 775 ${fn}
+sudo chown -R root:root ${fn}
+echo "権限はあっても所有権が無い場合"
+func
+
+sudo chmod -R 775 ${fn}
+sudo chown -R ebara:netmarketing ${fn}
+echo "所有権も権限もある場合"
+
