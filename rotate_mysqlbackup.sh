8bbf45121ca21834d9e88e9b9977a60fe4043969
ebara10 <chihiro.ebara@net-marketing.co.jp>
2015-04-17 18:41:12 +0900

    ログローテートアンチパターンOK

--- rotate_mysqlbackup.sh
+++ rotate_mysqlbackup.sh
@@ -6,41 +6,35 @@ EXPIRATIONDATE=`date "-d${TIME_LIMIT} days ago" '+%Y%m%d%H'`
 USER="root"
 PASSWORD="ned5725"
 DATABASE="symfony"
-FILENAME="${BACKUPDIR}${CURRENTTIME}"
+FILENAME="${CURRENTTIME}"".sql"
+LOG_DIR="/var/log/rotate.log"
+
+#バックアップフォルダがあるかの確認
+if [ ! -e ${BACKUPDIR} ]; then
+	echo "${BACKUPDIR}フォルダは存在しません。終了します"
+	exit 1
+fi
 
 #書き込みチェック
 touch ${BACKUPDIR}test.sql
 if [ $? -eq "1" ]; then
-	echo "書き込めませんでした。終了します。error1"
+	echo "書き込めませんでした。終了します。"
 	exit 1
 else
 	sudo rm -f ${BACKUPDIR}test.sql
 fi
-#バックアップフォルダがあるかの確認
-if [ ! -e ${BACKUPDIR} ]; then
-	echo "${BACKUPDIR}フォルダは存在しません。終了しますerror2"
-	exit 1
-fi
-#バックアップフォルダに書き込み権限があるかの確認
-if [ -w ${BACKUPDIR} ]; then
-	echo "write OK"
-else
-	echo "${BACKUPDIR}への書き込み権限がありません。終了しますerror3"
-	exit 1
-fi
+cd ${BACKUPDIR}
 
 #バックアップファイルの作成
-mysqldump -u ${USER} -p${PASSWORD} ${DATABASE} > ${FILENAME}.sql
+mysqldump -u ${USER} -p${PASSWORD} ${DATABASE} > ${FILENAME}
 #圧縮
-tar zcvf  ${FILENAME}.tar.gz ${FILENAME}.sql
-
-
-#バックアップファイル元の削除
-sudo rm -f ${FILENAME}.sql
+tar zcvf  ${CURRENTTIME}.tar.gz ${FILENAME}
+#圧縮前のバックアップファイル元の削除
+sudo rm -f ${FILENAME}
 
 #保存期間を過ぎたバックアップファイルの削除
-for BACKUP_FILE in `find ${BACKUPDIR} -name "*.sql"`;do
-    BACKUP_DATE=`echo ${BACKUP_FILE} | cut -c 9-18`
+for BACKUP_FILE in `find ${BACKUPDIR} -name "*.tar.gz"`;do
+    BACKUP_DATE=`cut -c 9-18 ${BACKUP_FILE}`
     #入力チェック
     if [[ ! ${BACKUP_DATE} =~ [0-9]{10} ]]; then
         continue;
@@ -49,4 +43,3 @@ for BACKUP_FILE in `find ${BACKUPDIR} -name "*.sql"`;do
         sudo rm -f ${BACKUP_FILE}
     fi
 done
-
