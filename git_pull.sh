a0ee831fe5cf1f4132b53a32b8d1304e8637e441
ebara10 <chihiro.ebara@net-marketing.co.jp>
2015-04-21 20:48:58 +0900

    デプロイスクリプトOK

--- git_pull.sh
+++ git_pull.sh
@@ -1,5 +1,38 @@
 #!/bin/sh
+GITDIR="/workspace/"
+PRODUCTDIR="/www/ebagraph/"
 
-cd /workspace/
+is_check_dir_exist(){
+#書き込み先、書き込みもとのフォルダがあるかを確認
+if [ ! -e $1 ]; then
+    echo "$1フォルダは存在しません。終了します"
+    exit 1
+fi
+echo "$1"
+}
+#戻り値チェック
+is_check_return_value(){
+if [[ $1 = 1 ]]; then
+    echo "書き込めませんでした。終了します"
+    exit 1;
+fi
+    return 0;
+}
+
+#ディレクトリチェック
+is_check_dir_exist ${GITDIR};
+is_check_dir_exist ${PRODUCTDIR};
+
+#コピーもとのフォルダに移動できるか
+cd ${GITDIR}
+is_check_return_value;
+
+#pull
 git pull
+#エラー時の処理
+is_check_return_value $?
+#コピー
+sudo cp -R ${GITDIR} ${PRODUCTDIR}
+
+#sudo cp -R /
 
--- rotate_mysqlbackup_timestamp.sh
+++ rotate_mysqlbackup_timestamp.sh
@@ -8,6 +8,15 @@ PASSWORD="ned5725"
 DATABASE="symfony"
 FILENAME="${CURRENTTIME}"".sql"
 
+#戻り値のチェック
+is_check_return_value(){
+if [ $? = 1 ]; then
+    echo "書き込めませんでした。終了します"
+    exit 1;
+fi
+    return 0;
+}
+
 #バックアップフォルダがあるかの確認
 if [ ! -e ${BACKUPDIR} ]; then
     echo "${BACKUPDIR}フォルダは存在しません。終了します"
@@ -15,29 +24,24 @@ if [ ! -e ${BACKUPDIR} ]; then
 fi
 
 #書き込み,日付チェックのため一時ファイルを作成
-sudo touch -d "$EXPIRATIONDATE" ${BACKUPDIR}tmp.txt
-if [ $? -eq "1" ]; then
-    echo "書き込めませんでした。終了します。"
-    exit 1
-fi
 cd ${BACKUPDIR}
+is_check_return_value;
 
 #バックアップファイルの作成
 mysqldump -u ${USER} -p${PASSWORD} ${DATABASE} > ${FILENAME}
+is_check_return_value;
+
 #圧縮
 tar zcvf  ${CURRENTTIME}.tar.gz ${FILENAME}
+is_check_return_value;
+
 #圧縮前のバックアップファイル元の削除
 sudo rm -f ${FILENAME}
 
 #保存期間を過ぎたバックアップファイルの削除
-for BACKUP_FILE in `find ${BACKUPDIR} -name "*.tar.gz"`;do
+for BACKUP_FILE in `find ${BACKUPDIR} -mtime +${TIME_LIMIT} -name "*.tar.gz"`;do
     #一時ファイルとファイルのリストを比較
-    if [[  ${BACKUP_FILE} -ot ${BACKUPDIR}tmp.txt ]]; then
-        sudo rm -f ${BACKUP_FILE}
-        echo "delete"${BACKUP_FILE}
-    fi
+#    sudo rm -f ${BACKUP_FILE}
+    echo "delete"${BACKUP_FILE}
 done
-
-#一時ファイルを削除
-rm -f ${BACKUPDIR}/tmp.txt
 
