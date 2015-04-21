24bfe5040c722a8c266906efa5aec0ab4f86cefa
ebara10 <chihiro.ebara@net-marketing.co.jp>
2015-04-21 16:53:39 +0900

    timestampOK

--- rotate_mysqlbackup_timestamp.sh
+++ rotate_mysqlbackup_timestamp.sh
@@ -10,15 +10,15 @@ FILENAME="${CURRENTTIME}"".sql"
 
 #バックアップフォルダがあるかの確認
 if [ ! -e ${BACKUPDIR} ]; then
-	echo "${BACKUPDIR}フォルダは存在しません。終了します"
-	exit 1
+    echo "${BACKUPDIR}フォルダは存在しません。終了します"
+    exit 1
 fi
 
 #書き込み,日付チェックのため一時ファイルを作成
 sudo touch -d "$EXPIRATIONDATE" ${BACKUPDIR}tmp.txt
 if [ $? -eq "1" ]; then
-	echo "書き込めませんでした。終了します。"
-	exit 1
+    echo "書き込めませんでした。終了します。"
+    exit 1
 fi
 cd ${BACKUPDIR}
 
@@ -31,11 +31,11 @@ sudo rm -f ${FILENAME}
 
 #保存期間を過ぎたバックアップファイルの削除
 for BACKUP_FILE in `find ${BACKUPDIR} -name "*.tar.gz"`;do
-	#一時ファイルとファイルのリストを比較
-	if [[  ${BACKUP_FILE} -ot ${BACKUPDIR}tmp.txt ]]; then
-		sudo rm -f ${BACKUP_FILE}
-		echo "delete"${BACKUP_FILE}
-	fi
+    #一時ファイルとファイルのリストを比較
+    if [[  ${BACKUP_FILE} -ot ${BACKUPDIR}tmp.txt ]]; then
+    sudo rm -f ${BACKUP_FILE}
+        echo "delete"${BACKUP_FILE}
+    fi
 done
 
 #一時ファイルを削除
