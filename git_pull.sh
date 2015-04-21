47254130b2c7ca383cd1ec724d47bc951da5121a
ebara10 <chihiro.ebara@net-marketing.co.jp>
2015-04-21 20:54:38 +0900

    デプロイスクリプト

--- git_pull.sh
+++ git_pull.sh
@@ -3,20 +3,19 @@ GITDIR="/workspace/"
 PRODUCTDIR="/www/ebagraph/"
 
 is_check_dir_exist(){
-#書き込み先、書き込みもとのフォルダがあるかを確認
-if [ ! -e $1 ]; then
-    echo "$1フォルダは存在しません。終了します"
-    exit 1
-fi
-echo "$1"
+    #書き込み先、書き込みもとのフォルダがあるかを確認
+    if [ ! -e $1 ]; then
+        echo "$1フォルダは存在しません。終了します"
+        exit 1
+    fi
 }
 #戻り値チェック
 is_check_return_value(){
-if [[ $1 = 1 ]]; then
-    echo "書き込めませんでした。終了します"
-    exit 1;
-fi
-    return 0;
+    if [[ $1 = 1 ]]; then
+        echo "書き込めませんでした。終了します"
+        exit 1;
+    fi
+        return 0;
 }
 
 #ディレクトリチェック
@@ -33,6 +32,3 @@ git pull
 is_check_return_value $?
 #コピー
 sudo cp -R ${GITDIR} ${PRODUCTDIR}
-
-#sudo cp -R /
-
