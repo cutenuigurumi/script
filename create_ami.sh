78d41c0c587a289e98451459432713e1c5755e92
ebara10 <chihiro.ebara@net-marketing.co.jp>
2015-04-28 16:25:17 +0900

    とりあえず

---
+++ create_ami.sh
@@ -0,0 +1,40 @@
+#!/bin/sh
+INSTANCE_ID="i-ca2a79d3"
+PREFIX="ebaraEC2-"
+CURRENTTIME=`date '+%Y%m%d%H%M'`
+LOGFILE="/tmp/create_ami.log"
+
+exec >> ${LOGFILE} 2>&1
+
+#以下chatwork api連携
+# nm-botのtoken
+TOKEN="ca89fefe62f2d5cd17da1e346a9961b3"
+# Room ID ブラウザの#!rid... ...の部分
+# ブランチ切り替えグループ
+ROOM="28508316"
+# API
+API_URL="https://api.chatwork.com/v1/rooms/$ROOM/messages"
+
+
+#戻り値のチェック
+is_check_return_value(){
+   if [ $? = 1 ]; then
+        echo "** `date '+%Y-%m-%d %H:%M:%S'` - START"
+        echo "** Create ami backup and rotate backup**"
+        echo "書き込めませんでした。終了します"
+        exit 1
+   fi
+return 0
+}
+
+RETURN=`aws ec2 create-image  --instance-id ${INSTANCE_ID} --name "${PREFIX}${CURRENTTIME}" --no-reboot`
+is_check_return_value
+
+
+echo "** `date '+%Y-%m-%d %H:%M:%S'` - END"
+
+LOG_DETAIL=`cat ${LOGFILE}`
+
+#chatworkに結果を連携
+RESULT=`curl -X POST -H "X-ChatWorkToken: $TOKEN" -d "body=${LOG_DETAIL}" $API_URL`
+cat /dev/null > ${LOGFILE}
--- deploy.sh
+++ deploy.sh
@@ -1,11 +1,11 @@
 #!/bin/sh
-PRODUCTDIR="/www/symfony${SOURCEDIR}"
-SOURCEDIR="/src"
-GITDIR="/workspace/symfony${SOURCEDIR}"
+#PRODUCTDIR="/www/symfony${SOURCEDIR}"
+#SOURCEDIR="/src"
+#GITDIR="/workspace/symfony${SOURCEDIR}"
 LOGFILE="/tmp/deploy.log"
 #debug用ディレクトリ
-#PRODUCTDIR="/www/test"
-#GITDIR="/workspace/test"
+PRODUCTDIR="/www/test"
+GITDIR="/workspace/test"
 
 #以下chatwork api連携
 # nm-botのtoken
@@ -14,10 +14,8 @@ TOKEN="ca89fefe62f2d5cd17da1e346a9961b3"
 ROOM="28508316"
 # API
 API_URL="https://api.chatwork.com/v1/rooms/$ROOM/messages"
+        exec >> ${LOGFILE} 2>&1
 
-exec >> ${LOGFILE} 2>&1
-echo "** `date '+%Y-%m-%d %H:%M:%S'` - START"
-echo "** deploy script report**"
 
 is_check_dir_exist(){
     #書き込み先、書き込みもとのフォルダがあるかを確認
@@ -33,7 +31,9 @@ is_check_dir_exist(){
 #戻り値チェック
 is_check_return_value(){
     if [[ $1 = 1 ]]; then
-        echo "失敗しました。終了します"
+        echo "** `date '+%Y-%m-%d %H:%M:%S'` - START"
+        echo "** deploy script report**"
+        echo "faild "
         exit 1;
     fi
     return 0;
@@ -51,7 +51,6 @@ git fetch
 #エラー時の処理
 is_check_return_value $?
 git merge origin/master
-
 #エラー時の処理
 is_check_return_value $?
 
@@ -64,5 +63,5 @@ LOG_DETAIL=`cat ${LOGFILE}`
 
 #chatworkに結果を連携
 RESULT=`curl -X POST -H "X-ChatWorkToken: $TOKEN" -d "body=${LOG_DETAIL}" $API_URL`
-cat /dev/null > ${LOGFILE}
+#cat /dev/null > ${LOGFILE}
 
