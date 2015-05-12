aa366b644bbd87c81c62b9b84f090d2014d0fffc
ebara10 <chihiro.ebara@net-marketing.co.jp>
2015-05-12 15:40:10 +0900

    ログの部分とか修正

--- backup_ami.sh
+++ backup_ami.sh
@@ -2,47 +2,58 @@
 PREFIX="ebaraEC2-"
 CURRENTTIME=`date '+%Y%m%d%H%M'`
 EXPIRATIONDATE=`date "-d${TIME_LIMIT} days ago" '+%Y%m%d%H%M'`
-LOGFILE="/tmp/create_ami.log"
+HEAD_LOGFILE="/tmp/head_create_ami.log"
+BODY_LOGFILE="/tmp/body_create_ami.log"
+END_LOGFILE="/tmp/end_create_ami.log"
+PREFIX="/backup/ebachannel_"
 
-cd /usr/local/aws/bin/
+cd /usr/local/aws/bin/ > ${BODY_LOGFILE} 2>&1
 #設定ファイル読み出し
 . ./setting.sh
 
-exec >> ${LOGFILE} 2>&1
+#ログのヘッダ部分作成
+echo "create ami backup and delete old ami" >> ${HEAD_LOGFILE} 2>&1
+echo "** `date '+%Y-%m-%d %H:%M:%S'` - START"  >> ${HEAD_LOGFILE} 2>&1
 
-echo "** `date '+%Y-%m-%d %H:%M:%S'` - START"
-# CHAT WORK API
-API_URL="https://api.chatwork.com/v1/rooms/$ROOM/messages"
-
+# API
+REGULAR_API_URL="https://api.chatwork.com/v1/rooms/${REGULAR_ROOM}/messages"
+ERROR_API_URL="https://api.chatwork.com/v1/rooms/${ERROR_ROOM}/messages"
 
 #戻り値のチェック
 is_check_return_value(){
    if [ $? = 1 ]; then
-        echo "エラーが発生しました。終了します"
+        echo "エラーが発生しました。終了します" >> ${BODY_LOGFILE} 2>&1
+        LOG_DETAIL=`cat ${HEAD_LOGFILE} ${BODY_LOGFILE} ${END_LOGFILE}`
+        #異常ログをchatworkに連携
+        RESULT=`curl -X POST -H "X-ChatWorkToken: $TOKEN" -d "body=${LOG_DETAIL}" $ERROR_API_URL`
+        delete_logfile
         exit 1
    fi
 return 0
 }
+delete_logfile(){
+    cat /dev/null > ${HEAD_LOGFILE}
+    cat /dev/null > ${BODY_LOGFILE}
+    cat /dev/null > ${END_LOGFILE}
+}
 
 #amiを作成
-echo "amiを作成"
-aws ec2 create-image  --instance-id ${INSTANCE_ID} --name "${PREFIX}${CURRENTTIME}" --no-reboot
-is_check_return_value
+echo "amiを作成" >> ${BODY_LOGFILE} 2>&1
+aws ec2 create-image  --instance-id ${INSTANCE_ID} --name "${PREFIX}${CURRENTTIME}" --no-reboot >> ${BODY_LOGFILE} 2>&1
 
 #amiの一覧を取得
 AMI_ID=`aws ec2 describe-images --owners self --filters "Name=name,Values=${PREFIX}${EXPIRATIONDATE}" | jq '.Images[]' | jq -r '.ImageId'`
 echo ${AMI_ID}
 if [ -z ${AMI_ID} ]; then
-    echo "削除するものはありませんでした。"
+    echo "削除するものはありませんでした。" >> ${BODY_LOGFILE} 2>&1
 else
     #削除
-    aws ec2 deregister-image --image-id ${AMI_ID}
+    aws ec2 deregister-image --image-id ${AMI_ID} >> ${BODY_LOGFILE} 2>&1
     is_check_return_value
-    echo "delete ami ${AMI_ID}"
+    echo "deleted-> ${AMI_ID}" > ${BODY_LOGFILE} 2>&1
 fi
-LOG_DETAIL=`cat ${LOGFILE}`
-
+LOG_DETAIL=`cat ${HEAD_LOGFILE} ${BODY_LOGFILE}`
 
 #chatworkに結果を連携
-RESULT=`curl -X POST -H "X-ChatWorkToken: $TOKEN" -d "body=${LOG_DETAIL}" $API_URL`
-cat /dev/null > ${LOGFILE}
+RESULT=`curl -X POST -H "X-ChatWorkToken: $TOKEN" -d "body=${LOG_DETAIL}" $REGULAR_API_URL`
+delete_logfile
--- rotate_mysqlbackup.sh
+++ rotate_mysqlbackup.sh
@@ -9,57 +9,59 @@ BODY_LOGFILE="/tmp/body.log"
 END_LOGFILE="/tmp/end.log"
 PREFIX="/backup/ebachannel_"
 
-cd /usr/local/aws/bin/
+cd /usr/local/aws/bin/ >> ${BODY_LOGFILE} 2>&1
 #設定ファイル読み出し
 . ./setting.sh
-
-echo -e "** `date '+%Y-%m-%d %H:%M:%S'` - START
-** Create  mysqldump backup and rotate backup script**" >  ${HEAD_LOGFILE} 2>&1
-
 # API
 REGULAR_API_URL="https://api.chatwork.com/v1/rooms/${REGULAR_ROOM}/messages"
 ERROR_API_URL="https://api.chatwork.com/v1/rooms/${ERROR_ROOM}/messages"
 
+echo -e "** `date '+%Y-%m-%d %H:%M:%S'` - START
+** Create  mysqldump backup and rotate backup script**" >>  ${HEAD_LOGFILE} 2>&1
 
 #戻り値のチェック
 is_check_return_value(){
    if [ $? = 1 ]; then
-        echo -e "書き込めませんでした。終了します\n
-        ** `date '+%Y-%m-%d %H:%M:%S'` - END" > ${END_LOGFILE} 2>&1
-        LOG_DETAIL=`cat ${HEAD_LOGFILE}  ${BODY_LOGFILE} ${END_LOGFILE}`
-
+        echo -e "Error! 終了します。\n** `date '+%Y-%m-%d %H:%M:%S'` - END" >> ${END_LOGFILE} 2>&1
+        LOG_DETAIL=`cat ${HEAD_LOGFILE} ${BODY_LOGFILE} ${END_LOGFILE}`
+        echo ${LOG_DETAIL}
         #異常ログをchatworkに連携
         RESULT=`curl -X POST -H "X-ChatWorkToken: $TOKEN" -d "body=${LOG_DETAIL}" $ERROR_API_URL`
-        #cat /dev/null > ${LOGFILE}
+        delete_logfile
         exit 1
     fi
     return 0
 }
+delete_logfile(){
+    cat /dev/null > ${HEAD_LOGFILE}
+    cat /dev/null > ${BODY_LOGFILE}
+    cat /dev/null > ${END_LOGFILE}
+}
 #バックアップフォルダがあるかの確認
 if [ ! -e ${BACKUPDIR} ]; then
     echo "${BACKUPDIR}フォルダは存在しません。終了します\n
-    ** `date '+%Y-%m-%d %H:%M:%S'` - END" > ${END_LOGFILE} 2>&1
+    ** `date '+%Y-%m-%d %H:%M:%S'` - END" >> ${END_LOGFILE} 2>&1
     LOG_DETAIL=`cat ${HEAD_LOGFILE}  ${BODY_LOGFILE} ${END_LOGFILE}`
     #異常ログをchatworkに連携
     RESULT=`curl -X POST -H "X-ChatWorkToken: $TOKEN" -d "body=${LOG_DETAIL}" $ERROR_API_URL`
-    #cat /dev/null > ${LOGFILE}
     exit 1
 fi
 	
-cd ${BACKUPDIR}
+cd ${BACKUPDIR} >> ${BODY_LOGFILE} 2>&1
 is_check_return_value
 	
 #バックアップファイルの作成
-mysqldump --defaults-group-suffix=_host1 ${DATABASE} > ${FILENAME} >  ${BODY_LOGFILE} 2>&1
+mysqldump --defaults-group-suffix=_host1 ${DATABASE} >> ${FILENAME} >>  ${BODY_LOGFILE} >2
 is_check_return_value
 
 #圧縮
-echo "圧縮開始" >  ${BODY_LOGFILE} 2>&1
-tar zcvf  ${PREFIX}${CURRENTTIME}.tar.gz ${FILENAME} >  ${BODY_LOGFILE} 2>&1
+tar zcvf  ${PREFIX}${CURRENTTIME}.tar.gz ${FILENAME} >>  ${BODY_LOGFILE} >2
 is_check_return_value
+echo "create backup file->${PREFIX}${CURRENTTIME}.tar.gz"  >>  ${BODY_LOGFILE} 2>&1
+
 
 #圧縮前のバックアップファイル元の削除
-sudo rm -f ${FILENAME} > ${BODY_LOGFILE} 2>&1
+sudo rm -f ${FILENAME} >> ${BODY_LOGFILE} 2>&1
 #保存期間を過ぎたバックアップファイルの削除
 for BACKUP_FILE in `find ${BACKUPDIR} -name "*.tar.gz"`;do
     #prefix文字カウント
@@ -70,15 +72,15 @@ for BACKUP_FILE in `find ${BACKUPDIR} -name "*.tar.gz"`;do
         continue
     fi
     if [ ${BACKUP_DATE} -le ${EXPIRATIONDATE} ]; then
-        echo "delete ${BACKUP_FILE}" > ${BODY_LOGFILE} 2>&1
-        sudo rm -f ${BACKUP_FILE} > ${BODY_LOGFILE} 2>&1
+        echo "deleted-> ${BACKUP_FILE}" >> ${BODY_LOGFILE} 2>&1
+        sudo rm -f ${BACKUP_FILE} >> ${BODY_LOGFILE} 2>&1
     fi
 done
-echo "** `date '+%Y-%m-%d %H:%M:%S'` - END" > ${END_LOGFILE} 2>&1
 
-LOG_DETAIL=`cat ${HEAD_LOGFILE}  ${BODY_LOGFILE} ${END_LOGFILE}`
+echo "SUCCESS!** `date '+%Y-%m-%d %H:%M:%S'` - END" >> ${END_LOGFILE} 2>&1
+
+LOG_DETAIL=`cat ${HEAD_LOGFILE} ${BODY_LOGFILE} ${END_LOGFILE}`
 
 #正常ログをchatworkに連携
 RESULT=`curl -X POST -H "X-ChatWorkToken: $TOKEN" -d "body=${LOG_DETAIL}" ${REGULAR_API_URL}` > ${END_LOGFILE} 2>&1
-cat /dev/null > ${LOGFILE}
-
+delete_logfile
